8b1023a9fe91dd6823910809dc6a781169988dd4
Mostafa Shokry El-Kamel <mostafashokry2222@gmail.com>
2025-04-24 22:31:11 +0200

    main: add initial database setup

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const dotenv = require('dotenv');
+const connectDB = require('./src/config/db');
 const { createApp } = require('./src/app');
 
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
-
-const app = createApp();
-app.listen(PORT, () => {
-    console.log(`🚀 Server running at http://localhost:${PORT}`);
+// Connect to DB and start server
+connectDB().then(() => {
+    const app = createApp();
+    app.listen(PORT, () => {
+        console.log(`Server running at http://localhost:${PORT}`);
+    });
 });
 
