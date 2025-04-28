e639f7c115481bcbf13818ca0d2f99bb8ba5c424
Mostafa Shokry El-Kamel <mostafashokry2222@gmail.com>
2025-04-29 01:42:52 +0300

    enhance(security): check if the user in the token exists

--- Backend/src/middleware/authenticate.middleware.js
+++ Backend/src/middleware/authenticate.middleware.js
@@ -1,7 +1,8 @@
 const jwt = require('jsonwebtoken');
 const AppError = require('../utils/AppError');
+const { getUserById } = require('../modules/auth/auth.service');
 
-const authenticate = (req, res, next) => {
+const authenticate = async (req, res, next) => {
     const token = req.header('Authorization')?.replace('Bearer ', '');
 
     if (!token) {
@@ -10,7 +11,10 @@ const authenticate = (req, res, next) => {
 
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = decoded;
+        const userId = decoded.id;
+
+        const user = await getUserById(userId);
+        req.user = { id: user._id, role: user.role };
         next();
     } catch (error) {
         next(new AppError('Invalid or expired token', 401));
--- Backend/src/modules/auth/auth.service.js
+++ Backend/src/modules/auth/auth.service.js
@@ -41,7 +41,7 @@ const login = async ({ email, password }) => {
         throw new AppError('Invalid email or password', 401);
     }
 
-    const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, {
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
         expiresIn: process.env.JWT_EXPIRATION,
     });
 
